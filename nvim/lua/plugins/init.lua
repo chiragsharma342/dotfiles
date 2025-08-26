cf59c2bd0510fef0bfb0770685965b3fea8c7c5d
chiragsharma342 <chirag.sharma342@gmail.com>
2025-08-26 17:32:47 +0530

    add nvim directory to repo

---
+++ nvim/lua/plugins/init.lua
@@ -0,0 +1,47 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    opts = require "configs.conform",
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
+
+  {
+    'mrcjkb/rustaceanvim',
+    version = '^6', -- Recommended
+    lazy = false, -- This plugin is already lazy
+    ["rust-analyzer"] = {
+      cargo = {
+        allFeatures = true,
+      },
+    },
+  },
+
+  {
+    'rust-lang/rust.vim',
+    ft = "rust",
+    init = function()
+      vim.g.rustfmt_autosave = 1
+    end
+  },
+
+  -- test new blink
+  { import = "nvchad.blink.lazyspec" },
+
+   {
+   	"nvim-treesitter/nvim-treesitter",
+   	opts = {
+   		ensure_installed = {
+   			"vim", "lua", "vimdoc",
+        "html", "css", "rust"
+   		},
+   	},
+   },
+}
